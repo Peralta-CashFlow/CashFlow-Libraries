8e90bf3a5dd68f3574f60b47421a8501a972977e
ViniciusPeralta <vinicius-peralta@hotmail.com>
2025-03-13 22:28:41 -0300

    feat: create base request

---
+++ Commons-Lib/src/main/java/com/cashflow/commons/core/dto/request/BaseRequest.java
@@ -0,0 +1,34 @@
+package com.cashflow.commons.core.dto.request;
+
+public class BaseRequest<T> {
+
+    private final String jwtToken;
+
+    private final String language;
+
+    private final T request;
+
+    public BaseRequest(String jwtToken, String language, T request) {
+        this.jwtToken = jwtToken;
+        this.language = language;
+        this.request = request;
+    }
+
+    public BaseRequest(String language, T request) {
+        this.jwtToken = null;
+        this.language = language;
+        this.request = request;
+    }
+
+    public String getJwtToken() {
+        return jwtToken;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public T getRequest() {
+        return request;
+    }
+}
