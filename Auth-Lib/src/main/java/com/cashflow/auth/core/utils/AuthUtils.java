0342b9e64ccc14bb2a0db3ec6f7ef78d5a4e7470
ViniciusPeralta <vinicius-peralta@hotmail.com>
2025-03-27 19:31:33 -0300

    feat: unfiltering swagger endpoints

--- Auth-Lib/src/main/java/com/cashflow/auth/core/utils/AuthUtils.java
+++ Auth-Lib/src/main/java/com/cashflow/auth/core/utils/AuthUtils.java
@@ -2,13 +2,16 @@ package com.cashflow.auth.core.utils;
 
 import com.cashflow.auth.core.domain.enums.RoleEnum;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class AuthUtils {
 
-    private AuthUtils() {}
+    private AuthUtils() {
+    }
 
     public static String mapRoleListToString(List<RoleEnum> roles) {
         return roles.stream()
@@ -21,5 +24,17 @@ public class AuthUtils {
                 .map(RoleEnum::valueOf)
                 .toList();
     }
+
+    public static String[] whiteListEndpoints(String[] apiWhiteList) {
+        List<String> whiteListEndpoints = new ArrayList<>(Arrays.asList(apiWhiteList));
+        whiteListEndpoints.addAll(BASE_WHITELIST_ENDPOINTS);
+        return whiteListEndpoints.toArray(new String[0]);
+    }
+
+    private static final List<String> BASE_WHITELIST_ENDPOINTS = List.of(
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            "/swagger-ui.html"
+    );
 
 }
