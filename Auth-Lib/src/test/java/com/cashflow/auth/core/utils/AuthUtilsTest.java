7afadbbfb7ff1244a79fc0a0ed8ce5ba6237f46c
ViniciusPeralta <vinicius-peralta@hotmail.com>
2025-07-03 19:56:54 -0300

    test: get jwt secret key

--- Auth-Lib/src/test/java/com/cashflow/auth/core/utils/AuthUtilsTest.java
+++ Auth-Lib/src/test/java/com/cashflow/auth/core/utils/AuthUtilsTest.java
@@ -1,12 +1,19 @@
 package com.cashflow.auth.core.utils;
 
 import com.cashflow.auth.core.domain.enums.RoleEnum;
+import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import uk.org.webcompere.systemstubs.environment.EnvironmentVariables;
+import uk.org.webcompere.systemstubs.jupiter.SystemStubsExtension;
 
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SystemStubsExtension.class)
 class AuthUtilsTest {
 
     @Test
@@ -19,5 +26,24 @@ class AuthUtilsTest {
     void givenStringOfRoles_whenMapStringToRoleList_thenReturnsListOfRoles() {
         List<RoleEnum> result = AuthUtils.mapStringToRoleList("CASH_FLOW_BASICS,CASH_FLOW_BASICS");
         assertEquals(List.of(RoleEnum.CASH_FLOW_BASICS, RoleEnum.CASH_FLOW_BASICS), result);
+    }
+
+    @Test
+    void shouldReturnSecretKeyWhenEnvVariableIsSet(EnvironmentVariables environmentVariables) {
+        String secret = "12345678901234567890123456789012";
+        environmentVariables.set("JWT_SECRET", secret);
+        SecretKey key = AuthUtils.getJwtSecretKey();
+        assertNotNull(key);
+        assertEquals(
+                Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8)).getEncoded().length,
+                key.getEncoded().length
+        );
+    }
+
+    @Test
+    void shouldThrowExceptionWhenEnvVariableIsNotSet(EnvironmentVariables environmentVariables) {
+        environmentVariables.set("JWT_SECRET", "");
+        IllegalStateException exception = assertThrows(IllegalStateException.class, AuthUtils::getJwtSecretKey);
+        assertEquals("JWT_SECRET environment variable is not set", exception.getMessage());
     }
 }
