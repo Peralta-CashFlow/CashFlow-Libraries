f1d0d472ea2e2538ebfebbeb67dc61a1c53b8acb
ViniciusPeralta <vinicius-peralta@hotmail.com>
2025-07-04 16:51:36 -0300

    feat: add authorization handler

--- Exceptions-Lib/src/main/java/com/cashflow/exception/core/domain/mapper/ExceptionResponseMapper.java
+++ Exceptions-Lib/src/main/java/com/cashflow/exception/core/domain/mapper/ExceptionResponseMapper.java
@@ -2,14 +2,41 @@ package com.cashflow.exception.core.domain.mapper;
 
 import com.cashflow.exception.core.CashFlowException;
 import com.cashflow.exception.core.domain.dto.response.ExceptionResponse;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import java.util.Objects;
 
+/**
+ * ExceptionResponseMapper is a utility class that provides methods to convert different types of exceptions
+ * into a standardized ExceptionResponse format.
+ * <p>
+ * This class is used to ensure that all exceptions thrown in the application are mapped to a consistent response structure,
+ * which can be easily consumed by clients.
+ * </p>
+ *
+ * @see #fromCashFlowException(CashFlowException)
+ * @see #fromGenericException(Exception)
+ *
+ *
+ * @author Vinicius Peralta
+ * @since 1.0.0
+ */
 public class ExceptionResponseMapper {
 
     private ExceptionResponseMapper() {}
 
+    /**
+     * Converts a CashFlowException into an ExceptionResponse.
+     *
+     * @param cashFlowException the CashFlowException to convert
+     * @return an ExceptionResponse containing the details of the CashFlowException
+     *
+     * @see CashFlowException
+     * @see ExceptionResponse
+     *
+     * @since 1.0.0
+     */
     public static ExceptionResponse fromCashFlowException(CashFlowException cashFlowException) {
         return new ExceptionResponse(
                 cashFlowException.getTitle(),
@@ -19,6 +46,17 @@ public class ExceptionResponseMapper {
         );
     }
 
+    /**
+     * Converts a generic Exception into an ExceptionResponse.
+     *
+     * @param exception the Exception to convert
+     * @return an ExceptionResponse containing the details of the Exception
+     *
+     * @see Exception
+     * @see ExceptionResponse
+     *
+     * @since 1.0.0
+     */
     public static ExceptionResponse fromGenericException(Exception exception) {
         return new ExceptionResponse(
                 "Unexpected Error",
@@ -28,6 +66,17 @@ public class ExceptionResponseMapper {
         );
     }
 
+    /**
+     * Converts a MethodArgumentNotValidException into an ExceptionResponse.
+     *
+     * @param methodArgumentNotValidException the MethodArgumentNotValidException to convert
+     * @return an ExceptionResponse containing the details of the MethodArgumentNotValidException
+     *
+     * @see MethodArgumentNotValidException
+     * @see ExceptionResponse
+     *
+     * @since 1.0.0
+     */
     public static ExceptionResponse fromMethodArgumentNotValidException(
             MethodArgumentNotValidException methodArgumentNotValidException
     ) {
@@ -39,6 +88,28 @@ public class ExceptionResponseMapper {
                 message,
                 methodArgumentNotValidException.getClass().getName(),
                 methodArgumentNotValidException.getStackTrace()[0].getMethodName()
+        );
+    }
+
+    /**
+     * Converts an AuthorizationDeniedException into an ExceptionResponse.
+     *
+     * @param authorizationDeniedException the AuthorizationDeniedException to convert
+     * @return an ExceptionResponse containing the details of the AuthorizationDeniedException
+     *
+     * @see AuthorizationDeniedException
+     * @see ExceptionResponse
+     *
+     * @since 1.0.0
+     */
+    public static ExceptionResponse fromAuthorizationDeniedException(
+            AuthorizationDeniedException authorizationDeniedException
+    ) {
+        return new ExceptionResponse(
+                "Authorization Denied",
+                "You are not authorized to perform this action.",
+                authorizationDeniedException.getStackTrace()[0].getClassName(),
+                authorizationDeniedException.getStackTrace()[0].getMethodName()
         );
     }
 
--- Exceptions-Lib/src/main/java/com/cashflow/exception/core/handler/CashFlowExceptionHandler.java
+++ Exceptions-Lib/src/main/java/com/cashflow/exception/core/handler/CashFlowExceptionHandler.java
@@ -4,12 +4,41 @@ import com.cashflow.exception.core.CashFlowException;
 import com.cashflow.exception.core.domain.dto.response.ExceptionResponse;
 import com.cashflow.exception.core.domain.mapper.ExceptionResponseMapper;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+/**
+ * CashFlowExceptionHandler is a global exception handler for the CashFlow applicationS.
+ * It intercepts exceptions thrown by controllers and maps them to appropriate ExceptionResponse responses.
+ * <p>
+ * This class handles specific exceptions like CashFlowException, MethodArgumentNotValidException,
+ * and generic exceptions, providing a consistent response structure for clients.
+ * </p>
+ *
+ * @see #handleCashFlowException(CashFlowException)
+ * @see #handleGenericException(Exception)
+ * @see #handleMethodArgumentNotValidException(MethodArgumentNotValidException)
+ * @see #handleAuthorizationDeniedException(AuthorizationDeniedException) 
+ *
+ * @author Vinicius Peralta
+ * @since 1.0.0
+ */
 @org.springframework.web.bind.annotation.ControllerAdvice
 public class CashFlowExceptionHandler {
 
+    /**
+     * Handles CashFlowException and maps it to an ExceptionResponse.
+     *
+     * @param cashFlowException the CashFlowException to handle
+     * @return a ResponseEntity containing the ExceptionResponse with the appropriate HTTP status code
+     *
+     * @see CashFlowException
+     * @see ExceptionResponse
+     * @see ExceptionResponseMapper
+     *
+     * @since 1.0.0
+     */
     @ExceptionHandler(CashFlowException.class)
     public ResponseEntity<ExceptionResponse> handleCashFlowException(CashFlowException cashFlowException) {
         return ResponseEntity
@@ -17,6 +46,18 @@ public class CashFlowExceptionHandler {
                 .body(ExceptionResponseMapper.fromCashFlowException(cashFlowException));
     }
 
+    /**
+     * Handles generic exceptions and maps them to an ExceptionResponse.
+     *
+     * @param exception the Exception to handle
+     * @return a ResponseEntity containing the ExceptionResponse with a 500 Internal Server Error status code
+     *
+     * @see Exception
+     * @see ExceptionResponse
+     * @see ExceptionResponseMapper
+     *
+     * @since 1.0.0
+     */
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ExceptionResponse> handleGenericException(Exception exception) {
         return ResponseEntity
@@ -24,6 +65,18 @@ public class CashFlowExceptionHandler {
                 .body(ExceptionResponseMapper.fromGenericException(exception));
     }
 
+    /**
+     * Handles MethodArgumentNotValidException and maps it to an ExceptionResponse.
+     *
+     * @param methodArgumentNotValidException the MethodArgumentNotValidException to handle
+     * @return a ResponseEntity containing the ExceptionResponse with a 400 Bad Request status code
+     *
+     * @see MethodArgumentNotValidException
+     * @see ExceptionResponse
+     * @see ExceptionResponseMapper
+     *
+     * @since 1.0.0
+     */
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ExceptionResponse> handleMethodArgumentNotValidException(
             MethodArgumentNotValidException methodArgumentNotValidException
@@ -31,6 +84,27 @@ public class CashFlowExceptionHandler {
         return ResponseEntity
                 .status(400)
                 .body(ExceptionResponseMapper.fromMethodArgumentNotValidException(methodArgumentNotValidException));
+    }
+
+    /**
+     * Handles AuthorizationDeniedException and maps it to an ExceptionResponse.
+     *
+     * @param authorizationDeniedException the AuthorizationDeniedException to handle
+     * @return a ResponseEntity containing the ExceptionResponse with a 403 Forbidden status code
+     *
+     * @see AuthorizationDeniedException
+     * @see ExceptionResponse
+     * @see ExceptionResponseMapper
+     *
+     * @since 1.0.0
+     */
+    @ExceptionHandler(AuthorizationDeniedException.class)
+    public ResponseEntity<ExceptionResponse> handleAuthorizationDeniedException(
+            AuthorizationDeniedException authorizationDeniedException
+    ) {
+        return ResponseEntity
+                .status(403)
+                .body(ExceptionResponseMapper.fromAuthorizationDeniedException(authorizationDeniedException));
     }
 
 }
